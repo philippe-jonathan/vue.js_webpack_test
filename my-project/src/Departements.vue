637cc702c3d2b549f6f87ad80a7fb5537e0cd983
philippe-jonathan <philippe1jonathan@gmail.com>
2018-11-15 15:21:40 +0100

    first_commit

---
+++ my-project/src/Departements.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id='departements'>
+        <h1>coucou</h1>
+        <ul>
+            <li v-for="d in departements">{{ d.nom }}</li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+import $ from 'jquery'
+
+export default {
+    name: 'departements',
+    data() {
+        return {
+            departements: []
+        }
+    },
+    created: function() {
+        $.ajax('https://geo.api.gouv.fr/departements')
+        .done( function(d) {
+            this.departements = d
+        }.bind(this))
+    }
+}
+</script>
+
+<style>
+ #departements {
+     text-align: center;
+ }
+</style>
